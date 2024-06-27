a4bce3d1cb04a9a8fcc6fce9527f0b2c0f1d4a30
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-06-27 10:50:42 +0530

    singleton pattern Implemented

--- src/main/java/com/example/Logger.java
+++ src/main/java/com/example/Logger.java
@@ -1,8 +1,16 @@
 package com.example;
 
-public class Logger {
+public final class Logger {
 
-
+    private Logger() {
+    }
+    public static Logger INSTANCE;
+    public static Logger getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Logger();
+        }
+        return INSTANCE;
+    }
     void log(String logMessage) {
         System.out.println(logMessage);
     }
