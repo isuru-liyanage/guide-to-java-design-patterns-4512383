3a08c53593a1429ca3bac78c0d8aaf9aaac65cd5
isuru-liyanage <isuruliyanageofficial@gmail.com>
2024-06-27 10:27:03 +0530

    builder pattern Implemented

--- src/main/java/com/example/ItemRegistry.java
+++ src/main/java/com/example/ItemRegistry.java
@@ -4,13 +4,19 @@ public class ItemRegistry {
 
     public static void main(String[] args) {
 
-        var item1 = new StoreItem(
-                "Pretzel", 2.0, "A tasty snack", null, 7, null);
+        var item1 = new StoreItem.StoreItemBuilder("Bread", 200.0)
+                .longDescription("A loaf of bread that is perfect for sandwiches")
+                .shortDescription("A loaf of bread")
+                .packagingType("Plastic")
+                .stockAvailable(100)
+                .build();
 
-        var item2 = new StoreItem(
-                "Soup", 1.5, null,
-                "A meal that you can warm up at home. It can come in different flavours including tomato, chicken, and vegetable",
-                null, "Can");
+        var item2 = new StoreItem.StoreItemBuilder("Milk", 50.0)
+                .longDescription("A carton of milk that is perfect for breakfast")
+                .shortDescription("A carton of milk")
+                .packagingType("Glass")
+                .stockAvailable(50)
+                .build();
 
         addToItemRegistry(item1);
         addToItemRegistry(item2);
@@ -19,5 +25,6 @@ public class ItemRegistry {
     private static void addToItemRegistry(StoreItem storeItem) {
         System.out.println("A new item was added to the registry: \n" + storeItem);
     }
+
 
 }
